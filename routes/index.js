31fd63dedf7669f4f8c9eb20bf66a7f5456bc226
Rudy Perez <rrudy90023@gmail.com>
2014-11-29 18:55:01 -0800

    Route ids initial

--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,55 @@
 var express = require('express');
 var router = express.Router();
-
+var appdata = require('../data.json');
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'rudes.de' });
+  res.render('index', { title: 'Home' });
 });
 
 
 /* GET work page. */
 router.get('/work', function(req, res) {
-  res.render('work', { title: 'WORK' });
+	var myProjects = [];
+  //loop in the works object
+	appdata.works.forEach ( function (item){
+    //loop in more in the projects object
+    item.projects.forEach(function(project) {
+      myProjects = myProjects.concat(project["namecase"]);
+    });
+  });
+  res.render('work', { 
+  	title: 'Work',
+  	projects: myProjects
+  });
 });
 
 
-/* GET work page. */
+/* GET client page. */
+router.get('/work/:clientid', function(req, res) {
+  var myProjects = [];
+  appdata.works.forEach ( function (item){
+    //find what client matches what
+    if (item.client == req.params.clientid) {
+
+      item.projects.forEach(function(project) {
+      myProjects = myProjects.concat(project["namecase"]);
+    });
+
+    }
+
+  });
+  res.render('work', { 
+    title: 'Work',
+    projects: myProjects
+  });
+});
+
+
+
+
+/* GET profile page. */
 router.get('/profile', function(req, res) {
-  res.render('profile', { title: 'PROFILE' });
+  res.render('profile', { title: 'Profile' });
 });
 
 module.exports = router;
